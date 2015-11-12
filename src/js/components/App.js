798196b6b092af0bf5af16f3225eb08d99925446
sohilpandya <sohilpandya@me.com>
2015-11-12 12:52:00 +0000

    basic module

---
+++ src/js/components/App.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+
+
+var App = React.createClass({
+  render: function () {
+    return(
+      <div className='app-container'>
+        <h1>Hello World!</h1>
+
+      </div>
+
+    );
+  }
+});
+
+module.exports = App;
